9d3024cc1c0384ff458b5471ee93e63809c5c2b6
Ekin Böke <eboke16@ku.edu.tr>
2020-12-16 10:50:49 +0300

    Update __init__.py

--- boke/utils/__init__.py
+++ boke/utils/__init__.py
@@ -2,7 +2,7 @@ from utils.models import load_model, MFCC13, process_data
 
 import sys
 sys.modules['__main__'].MFCC13 = MFCC13
-model, preprocessor = load_model('utils/CNN-MODEL-2.h5')
+model, preprocessor = load_model('utils/SVM-best.model')
 
 from functools import partial
 preprocessor = partial(process_data, preprocessor=preprocessor)
